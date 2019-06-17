8f7a5e63156bc3cea66525bba1ccd62026b52ee4
Maks <petrenkomaks405@gmail.com>
2019-06-17 16:00:12 +0300

    Task 3

---
+++ src/Task3.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Task3 {
+public static void main(String []args){
+Scanner s =new Scanner (System.in);
+String x = s.nextLine();
+String y = s.nextLine();
+String k =s.nextLine();
+String g = s.nextLine();
+String l = s.nextLine();
+String d = s.nextLine();
+String f = s.nextLine();
+String a = s.nextLine();
+
+
+
+
+System.out.println("\nint = "+x);
+System.out.println("float = "+y);
+System.out.println("double = "+k);
+System.out.println("char = "+g);
+System.out.println("long = "+l);
+System.out.println("boolean = "+d);
+System.out.println("byte = "+f);
+System.out.println("short = "+a);
+    }
+}
